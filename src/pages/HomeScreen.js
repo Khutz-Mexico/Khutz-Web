20ea644fe6f45939316b3e010be400fbad3c324b
Leandro Cotti <lefcott@hotmail.com>
2022-03-08 00:52:14 -0300

    add title and dark mode

--- src/pages/HomeScreen.js
+++ src/pages/HomeScreen.js
@@ -2,8 +2,16 @@ import React from 'react';
 
 export const HomeScreen = () => {
   return (
-    <div>
-      <h1 className="text-3xl">Home Screen</h1>
+    <div className="flex flex-col items-center mt-[145px]">
+      <div className="flex flex-col leading-[85px]">
+        <p className="text-[70px] font-semibold text-center">AYUDAMOS A LAS</p>
+        <p className="text-[70px] font-semibold text-center">
+          MARCAS A <span className="secondaryColor">PROSPERAR</span>
+        </p>
+        <p className="text-[70px] font-semibold text-center">
+          EN LA ERA DIGITAL
+        </p>
+      </div>
     </div>
   );
 };
