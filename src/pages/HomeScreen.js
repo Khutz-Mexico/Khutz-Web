5d03f71c8e912dda14a87c69517123cf62ea1e35
Oscar Estrada <arturostrada2020@gmail.com>
2022-03-07 22:31:09 -0600

    logo and icons added in navbar

--- src/pages/HomeScreen.js
+++ src/pages/HomeScreen.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const HomeScreen = () => {
   return (
-    <div>
+    <div className="h-[2000px]">
       <h1 className="text-3xl">Home Screen</h1>
     </div>
   );
