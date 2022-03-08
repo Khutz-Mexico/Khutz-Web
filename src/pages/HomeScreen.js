93d3663c590f734da75ee801b227de81f753f23e
Leandro Cotti <lefcott@hotmail.com>
2022-03-08 01:42:16 -0300

    responsive adjustments

--- src/pages/HomeScreen.js
+++ src/pages/HomeScreen.js
@@ -4,16 +4,12 @@ import Button from '../components/Button';
 export const HomeScreen = () => {
   return (
     <div className="flex flex-col items-center mt-14 md:mt-[145px]">
-      <div className="flex flex-col leading-[85px]">
-        <p className="text-3xl md:text-[70px] font-semibold text-center">
-          AYUDAMOS A LAS
-        </p>
-        <p className="text-3xl md:text-[70px] font-semibold text-center">
+      <div className="flex flex-col text-3xl md:text-[70px] md:leading-[85px]">
+        <p className="font-semibold text-center">AYUDAMOS A LAS</p>
+        <p className="font-semibold text-center">
           MARCAS A <span className="secondaryColor">PROSPERAR</span>
         </p>
-        <p className="text-3xl md:text-[70px] font-semibold text-center">
-          EN LA ERA DIGITAL
-        </p>
+        <p className="font-semibold text-center">EN LA ERA DIGITAL</p>
       </div>
       <div className="w-60 mt-[70px]">
         <Button rightArrow solid>
