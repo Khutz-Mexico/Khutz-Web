047ad0acadd21a3b3c3ffe658748dc2d59228ad2
Leandro Cotti <lefcott@hotmail.com>
2022-03-08 01:39:36 -0300

    added button, installed hero icons

--- src/pages/HomeScreen.js
+++ src/pages/HomeScreen.js
@@ -1,16 +1,24 @@
 import React from 'react';
+import Button from '../components/Button';
 
 export const HomeScreen = () => {
   return (
-    <div className="flex flex-col items-center mt-[145px]">
+    <div className="flex flex-col items-center mt-14 md:mt-[145px]">
       <div className="flex flex-col leading-[85px]">
-        <p className="text-[70px] font-semibold text-center">AYUDAMOS A LAS</p>
-        <p className="text-[70px] font-semibold text-center">
+        <p className="text-3xl md:text-[70px] font-semibold text-center">
+          AYUDAMOS A LAS
+        </p>
+        <p className="text-3xl md:text-[70px] font-semibold text-center">
           MARCAS A <span className="secondaryColor">PROSPERAR</span>
         </p>
-        <p className="text-[70px] font-semibold text-center">
+        <p className="text-3xl md:text-[70px] font-semibold text-center">
           EN LA ERA DIGITAL
         </p>
+      </div>
+      <div className="w-60 mt-[70px]">
+        <Button rightArrow solid>
+          Nuestro trabajo
+        </Button>
       </div>
     </div>
   );
