7932020eca06d78b1c60c7025d559040de4f05c2
Oscar Estrada <arturostrada2020@gmail.com>
2022-03-08 13:50:46 -0600

    links added to MovilPopup

--- src/components/MovilPopup.js
+++ src/components/MovilPopup.js
@@ -3,7 +3,23 @@ import React from 'react';
 export const MovilPopup = () => {
   return (
     <div className="absolute right-0 bottom-14 w-[250px] rounded-l-2xl h-[250px] bg-[#1B212C]">
-      <div></div>
+      <ul className="flex flex-col w-full h-full items-start justify-between pr-8 py-7">
+        <li className="text-white text-[20px] w-full hover:bg-slate-700 rounded-r-xl pl-8">
+          Home
+        </li>
+        <li className="text-white text-[20px] w-full hover:bg-slate-700 rounded-r-xl pl-8">
+          Portafolio
+        </li>
+        <li className="text-white text-[20px] w-full hover:bg-slate-700 rounded-r-xl pl-8">
+          Servicios
+        </li>
+        <li className="text-white text-[20px] w-full hover:bg-slate-700 rounded-r-xl pl-8">
+          Cotizar
+        </li>
+        <li className="text-white text-[20px] w-full hover:bg-slate-700 rounded-r-xl pl-8">
+          Contacto
+        </li>
+      </ul>
     </div>
   );
 };
