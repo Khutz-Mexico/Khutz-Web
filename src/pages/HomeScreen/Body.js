3360be93dd69fde47f88bd1dccf1e394630ba469
Leandro Cotti <lefcott@hotmail.com>
2022-03-09 00:23:13 -0300

    finished bofdy

--- src/pages/HomeScreen/Body.js
+++ src/pages/HomeScreen/Body.js
@@ -5,7 +5,7 @@ const Body = () => {
   const { t } = useTranslation();
 
   return (
-    <div className="flex justify-evenly mt-[447px]">
+    <div className="flex justify-center items-center space-x-[133px] w-full mt-[447px]">
       <div className="flex flex-col items-stretch space-y-[36px]">
         <div>
           <p className="text-[32px] font-bold">{t('home.body.howCanWe')}</p>
@@ -24,7 +24,49 @@ const Body = () => {
           <p>{t('home.body.paragraph2.line5')}</p>
         </div>
         <div className="w-[100px]">
-          <Button rightArrow>{t('home.body.services')}</Button>
+          <Button rightArrow>
+            <div className="font-semibold">{t('home.body.services')}</div>
+          </Button>
+        </div>
+      </div>
+      <div className="flex justify-between space-x-[45px]">
+        <div className="flex flex-col">
+          <p className="secondaryColor text-[13px] font-bold mb-1">
+            {t('home.body.servicesSection.design.title')}
+          </p>
+          <div className="montserrat-font flex flex-col space-y-2">
+            <p className="primaryColor text-sm font-semibold">
+              {t('home.body.servicesSection.design.uxUiDesign')}
+            </p>
+            <p className="primaryColor text-sm font-semibold">
+              {t('home.body.servicesSection.design.prototyping')}
+            </p>
+            <p className="primaryColor text-sm font-semibold">
+              {t('home.body.servicesSection.design.branding')}
+            </p>
+            <p className="primaryColor text-sm font-semibold">
+              {t('home.body.servicesSection.design.illustration')}
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-col">
+          <p className="secondaryColor text-[13px] font-bold mb-1">
+            {t('home.body.servicesSection.development.title')}
+          </p>
+          <div className="montserrat-font flex flex-col space-y-2">
+            <p className="primaryColor text-sm font-semibold">
+              {t('home.body.servicesSection.development.webDevelopment')}
+            </p>
+            <p className="primaryColor text-sm font-semibold">
+              {t('home.body.servicesSection.development.apps')}
+            </p>
+            <p className="primaryColor text-sm font-semibold">
+              {t('home.body.servicesSection.development.crmAndErp')}
+            </p>
+            <p className="primaryColor text-sm font-semibold">
+              {t('home.body.servicesSection.development.cms')}
+            </p>
+          </div>
         </div>
       </div>
     </div>
