f82ea544f486dacc220cf02d57418227013f182f
Oscar Estrada <arturostrada2020@gmail.com>
2022-03-08 20:00:32 -0600

    hide scroll bar

--- src/pages/PortfolioScreen.js
+++ src/pages/PortfolioScreen.js
@@ -3,12 +3,21 @@ import { LargeCard } from '../components/LargeCard';
 
 export const PortfolioScreen = () => {
   return (
-    <div className="flex overflow-scroll scrollbar-hide">
-      <div className="flex space-x-5 bg-red-100">
-        {projects?.map(({ title, service }) => (
-          <LargeCard key={title} cardTitle={title} cardService={service} />
-        ))}
+    <>
+      <div className="flex flex-col justify-between h-[calc(100vh-var(--navbar-height))] w-full overflow-hidden pb-[40px] pt-[20px]">
+        {/* cards */}
+        <div className="flex overflow-scroll scrollbar-hide">
+          <div className="flex space-x-10 first-of-type:ml-[40px]">
+            {projects?.map(({ title, service }) => (
+              <LargeCard key={title} cardTitle={title} cardService={service} />
+            ))}
+          </div>
+        </div>
+        {/* Footer */}
+        <div className="flex w-full px-8 md:px-[40px]">
+          <h2>hola mundo</h2>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
