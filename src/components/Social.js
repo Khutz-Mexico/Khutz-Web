e7cf9708571a3c72dec4cd66006c4987c18c98c7
Oscar Estrada <arturostrada2020@gmail.com>
2022-03-15 17:06:02 -0600

    merge problems fixed

--- src/components/Social.js
+++ src/components/Social.js
@@ -3,16 +3,18 @@ import { BsBehance, BsGithub, BsInstagram } from 'react-icons/bs';
 
 const Social = () => {
   return (
-    <div className="flex flex-col justify-center space-y-5 h-[200px]">
-      <a
-        href="https://www.instagram.com/khutzmexico/"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <BsInstagram className="h-7 fill-[#909090] hover:fill-[var(--primaryColor)]" />
-      </a>
-      <BsBehance className="h-7 fill-[#909090] hover:fill-[var(--primaryColor)]" />
-      <BsGithub className="h-7 fill-[#909090] hover:fill-[var(--primaryColor)]" />
+    <div className="fixed left-6 md:left-[40px] top-[calc(50%-100px)]">
+      <div className="flex flex-col justify-center space-y-5 h-[200px]">
+        <a
+          href="https://www.instagram.com/khutzmexico/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BsInstagram className="h-7 fill-[#909090] hover:fill-[var(--primaryColor)]" />
+        </a>
+        <BsBehance className="h-7 fill-[#909090] hover:fill-[var(--primaryColor)]" />
+        <BsGithub className="h-7 fill-[#909090] hover:fill-[var(--primaryColor)]" />
+      </div>
     </div>
   );
 };
