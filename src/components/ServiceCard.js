9e51aaab6272a6266b690e154cfd3c3bfa7e9196
Leandro Cotti <lefcott@hotmail.com>
2022-03-15 22:42:37 -0300

    added a little animation to the service cards

--- src/components/ServiceCard.js
+++ src/components/ServiceCard.js
@@ -1,6 +1,6 @@
 const ServiceCard = ({ title, text }) => {
   return (
-    <div className="filled flex flex-col items-start justify-evenly p-10 space-y-9">
+    <div className="filled flex flex-col items-start justify-evenly p-10 space-y-9 cursor-default select-none duration-300 hover:scale-110">
       <h1 className="secondaryColor text-base font-bold">{title}</h1>
       <p className="text-base font-semibold">{text}</p>
     </div>
