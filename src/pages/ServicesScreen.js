b3a02f3b2035b6d744cfbfe4b21322af9012ee13
Leandro Cotti <lefcott@hotmail.com>
2022-03-15 23:07:51 -0300

    added sections to services page

--- src/pages/ServicesScreen.js
+++ src/pages/ServicesScreen.js
@@ -2,14 +2,17 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import Social from 'components/Social';
 import ServiceCard from 'components/ServiceCard';
+import RecentWork from './HomeScreen/RecentWork';
+import WorkToghether from './HomeScreen/WorkToghether';
+import Footer from 'components/Footer';
 
 export const ServicesScreen = () => {
   const { t } = useTranslation();
   const services = t('servicesScreen.services', { returnObjects: true });
 
   return (
-    <div className="filled2">
-      <div className="screen-height pl-[100px] pt-[100px] pr-[139px]">
+    <div>
+      <div className="screen-height filled2 pl-[100px] pt-[100px] pr-[139px]">
         <div className="pl-[134px]">
           <p className="quaternaryColor text-xs font-semibold">
             {t('servicesScreen.subtitle')}
@@ -23,8 +26,14 @@ export const ServicesScreen = () => {
             <ServiceCard title={service.title} text={service.text} key={i} />
           ))}
         </div>
-        <Social />
       </div>
+
+      <div className="pt-[110px]">
+        <RecentWork />
+      </div>
+      <WorkToghether />
+      <Social />
+      <Footer />
     </div>
   );
 };
