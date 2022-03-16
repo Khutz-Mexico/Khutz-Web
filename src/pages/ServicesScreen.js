035cf66d183c46de88e35bbb095d1e013cc7da5a
Leandro Cotti <lefcott@hotmail.com>
2022-03-15 22:33:19 -0300

    removed unnecessary services constants

--- src/pages/ServicesScreen.js
+++ src/pages/ServicesScreen.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import Social from 'components/Social';
-import { services } from 'data/database';
 import ServiceCard from 'components/ServiceCard';
 
 export const ServicesScreen = () => {
   const { t } = useTranslation();
+  const services = t('servicesScreen.services', { returnObjects: true });
 
   return (
     <div className="filled2">
@@ -20,11 +20,7 @@ export const ServicesScreen = () => {
         </div>
         <div className="grid grid-rows-2 grid-cols-3 gap-x-[42px] gap-y-[50px] mt-[104px]">
           {services.map((service, i) => (
-            <ServiceCard
-              title={t(`servicesScreen.services.${i}.title`)}
-              text={t(`servicesScreen.services.${i}.text`)}
-              key={i}
-            />
+            <ServiceCard title={service.title} text={service.text} key={i} />
           ))}
         </div>
         <Social />
